4baa0dcd5a1fc4e310c8224a76d611f99342e5a3
Youssef Mostafa <y.ismail@tekunda.com>
2025-06-25 00:18:57 +0300

    feature/ add shoppingCart

--- payment.py
+++ payment.py
@@ -1,14 +1,14 @@
 from abc import ABC, abstractmethod
-class Payment(ABC):
+class payment(ABC):
     """Abstract base for payment methods."""
     @abstractmethod
     def pay(self, amount: float) :
         pass
 
-class StripeProcessor(Payment):
+class StripeProcessor(payment):
     def pay(self, amount: float):
         print(f"[Stripe] Charged ${amount:.2f}")
 
-class PaypalProcessor(Payment):
+class PaypalProcessor(payment):
     def pay(self, amount: float):
         print(f"[PayPal] Charged ${amount:.2f}")
